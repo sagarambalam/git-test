916f7a5472d2a7c5ed1b6ff28a59d4891c683736
sagarambalam <sagar.ambalam@gmail.com>
2018-03-15 11:07:22 +0530

    Done

---
+++ haptik-challenge/build.py
@@ -0,0 +1,22 @@
+def feature_target_separator(dataset):
+    """
+    enter your solution here
+    """
+
+
+def reverse_OHE(df):
+    """
+    enter your solution here
+    """
+
+
+def word_cloud(X, max_font_size):
+    """
+    enter your solution here
+    """
+
+
+def classification(X_train, X_test, y_train, y_test, tokenizer, stop_words, ngram_range, max_df, min_df):
+    """
+    enter your solution here
+    """
---
+++ haptik-challenge/tests/test_classification.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestClassification(TestCase):
+    def test_classification(self):
+        self.assertTrue(True)
